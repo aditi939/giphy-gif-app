0354879d48a19ca3b2b59944342ae04a8d4702ec
aditi939 <paladiti939@gmail.com>
2023-08-31 23:35:23 +0530

    remove comment

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   const pages = [];
 
-  // Calculate the range of pages to display (e.g., 1, 2, 3 for the first set)
   const rangeStart = Math.max(currentPage - 1, 1);
   const rangeEnd = Math.min(currentPage + 1, totalPages);
 
