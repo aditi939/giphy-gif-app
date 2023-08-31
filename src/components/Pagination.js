b3ea1a93e1fd3bf0b09cf53bff9b3317522f2524
aditi939 <paladiti939@gmail.com>
2023-08-31 22:28:15 +0530

    changes pagination functionalities

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,34 +1,3 @@
-
-// import React from 'react';
-
-// const Pagination = ({ currentPage, totalPages, onPageChange }) => {
-//   const pageNumbers = [];
-
-//   for (let i = 1; i <= totalPages; i++) {
-//     pageNumbers.push(i);
-//   }
-
-//   return (
-//     <ul className="pagination">
-//       {pageNumbers.map((number) => (
-//         <li
-//           key={number}
-//           className={`page-item ${number === currentPage ? 'active' : ''}`}
-//         >
-//           <button
-//             onClick={() => onPageChange(number)}
-//             className="page-link"
-//           >
-//             {number}
-//           </button>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
-
-// export default Pagination;
-
 import React from 'react';
 
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
@@ -54,4 +23,5 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
 };
 
 export default Pagination;
+
 
