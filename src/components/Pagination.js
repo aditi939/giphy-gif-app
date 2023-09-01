5a094509bcdfda2dfca0acc396b24905022f2d5a
aditi939 <paladiti939@gmail.com>
2023-09-01 11:56:03 +0530

    add arrow button in pagination component

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -6,6 +6,15 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   const rangeStart = Math.max(currentPage - 1, 1);
   const rangeEnd = Math.min(currentPage + 1, totalPages);
 
+  // Add start arrow button if currentPage is greater than 2
+  if (currentPage > 2) {
+    pages.push(
+      <button key={1} onClick={() => onPageChange(1)}>
+        {'<<'}
+      </button>
+    );
+  }
+
   for (let page = rangeStart; page <= rangeEnd; page++) {
     pages.push(
       <button
@@ -18,8 +27,22 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
     );
   }
 
+  // Add end arrow button if currentPage is less than totalPages - 1
+  if (currentPage < totalPages - 1) {
+    pages.push(
+      <button key={totalPages} onClick={() => onPageChange(totalPages)}>
+        {'>>'}
+      </button>
+    );
+  } else if (currentPage === totalPages - 1) {
+    pages.push(
+      <button key={totalPages} onClick={() => onPageChange(totalPages)}>
+        {totalPages}
+      </button>
+    );
+  }
+
   return <div className="pagination">{pages}</div>;
 };
 
 export default Pagination;
-
